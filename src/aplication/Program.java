671845e47fea0672a8fd5368155eadaaf67dec69
rodrigodojo <rodrigodoj@gmail.com>
2021-01-28 11:09:53 -0300

    Exemplo de try-catch atualizado

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -7,6 +7,20 @@ public class Program {
 
 	public static void main(String[] args) {
 		
+		method1();
+		
+		System.out.println("End of program.");
+		
+	}
+	
+	public static void method1() {
+		System.out.println("****METHOD1 START****");
+		method2();
+		System.out.println("****METHOD1 END****");
+	}
+	
+	public static void method2() {
+		System.out.println("****METHOD2 START****");
 		Scanner sc = new Scanner(System.in);
 		
 		try {
@@ -16,14 +30,15 @@ public class Program {
 		}
 		catch(ArrayIndexOutOfBoundsException e) {
 			System.out.println("Invalid Position");
+			e.printStackTrace(); // ajuda a analisar o erro
+			sc.next();
 		}
 		catch(InputMismatchException e) {
 			System.out.println("Input error");
 		}
 		
-		System.out.println("End of program.");
-		
 		sc.close();
+		System.out.println("****METHOD2 END****");
 	}
 
 }
